4edd7a0b81f04c2ef0bc62b2b8840abe16f9b2cb
shir15a <shir95admon@gmail.com>
2021-03-23 13:59:30 +0200

    14.2

--- src/Components/Copy_14.2/Copy.js
+++ src/Components/Copy_14.2/Copy.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react'
 
 export default class Copy extends Component {
-    state = { val: '' }
+    myInput = React.createRef();
 
-    c = (e) => {
-        console.log(e.target.value);
-        this.setState({ val: e.target.value })
+    copy = () => {
+        this.myInput.current.select();
+        document.execCommand('copy');
+        document.execCommand('delete');
     }
 
-    copy = ()=>{
-        console.log(this.state.val);
-    }
     render() {
         return (
             <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
-                <textarea className="textarea" name="textarea" rows="4" cols="50" onChange={this.c}></textarea>
+                <textarea className="textarea" name="textarea" rows="4" cols="50" ref={this.myInput}></textarea>
                 <button onClick={this.copy}>copy</button>
             </div>
         )
     }
 }
+
+
+
+
+
+// import React, { Component } from 'react'
+
+// export default class Copy extends Component {
+//     state = { val: '' }
+
+//     c = (e) => {
+//         console.log(e.target.value);
+//         this.setState({ val: e.target.value })
+//     }
+
+//     copy = ()=>{
+//         console.log(this.state.val);
+//     }
+//     render() {
+//         return (
+//             <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
+//                 <textarea className="textarea" name="textarea" rows="4" cols="50" onChange={this.c}></textarea>
+//                 <button onClick={this.copy}>copy</button>
+//             </div>
+//         )
+//     }
+// }
