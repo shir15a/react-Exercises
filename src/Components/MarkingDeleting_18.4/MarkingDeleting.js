090c02e8a76f8f1a640145f8d346c9089c9d049f
shir15a <shir95admon@gmail.com>
2021-03-29 15:21:13 +0300

    18.4

--- src/Components/MarkingDeleting_18.4/MarkingDeleting.js
+++ src/Components/MarkingDeleting_18.4/MarkingDeleting.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 
+const arr = ["one", "two", "three", "four", "five"];
+
+
 const MarkingDeleting = () => {
 
-    const arr = ["one", "two", "three", "four", "five"];
+    const [data, setData] = useState(arr);
 
     return (
         <div>
             <button>Delete</button>
             <button>Reset</button><br></br>
-            {arr.map((number, index)=>{
+            {data.map((number, index)=>{
                 return (<div>
                     <input type='checkbox' id={index} name= {number}></input>
                     <label> {arr[index]}</label><br></br>
