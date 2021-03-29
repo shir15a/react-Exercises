0de9d6795f1a8270343a62489eaea64ac814b79e
shir15a <shir95admon@gmail.com>
2021-03-29 21:22:27 +0300

    18.4

--- src/Components/MarkingDeleting_18.4/MarkingDeleting.js
+++ src/Components/MarkingDeleting_18.4/MarkingDeleting.js
@@ -1,23 +1,40 @@
 import React, { useState } from 'react';
 
-const arr = ["one", "two", "three", "four", "five"];
-
+const numbers = ["one", "two", "three", "four", "five"];
 
 const MarkingDeleting = () => {
 
-    const [data, setData] = useState(arr);
+    const [data, setData] = useState(numbers.map(number => ({number, checked: false})));
+    
+    const resetCheckbox = () => {
+        setData(numbers.map(item => ({number: item, checked: false})));
+    }
+
+    const deleteCheckbox = () => {
+        const newData = data.filter(item => (!item.checked));
+        setData(newData);
+    }
+
+    const checkboxChanged = (index) => {
+        const newData = [...data];
+        newData[index].checked = newData[index].checked ? false : true;
+        setData(newData);
+    }
+    
+
+   
 
     return (
         <div>
-            <button>Delete</button>
-            <button>Reset</button><br></br>
-            {data.map((number, index)=>{
+            <button onClick={deleteCheckbox}>Delete</button>
+            <button onClick={resetCheckbox}>Reset</button><br></br>
+            {data.map((item, index) => {
                 return (<div>
-                    <input type='checkbox' id={index} name= {number}></input>
-                    <label> {arr[index]}</label><br></br>
-                    </div>)
+                    <input type='checkbox' key={index} name={item.number} checked={item.checked} onChange={() => checkboxChanged(index)}></input>
+                    <label> {item.number}</label><br></br>
+                </div>)
             })}
-    </div>
+        </div>
     )
 }
 
