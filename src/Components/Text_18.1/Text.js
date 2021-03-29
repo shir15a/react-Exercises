582c21262c7c995a84340a17fc50c478e074ac90
shir15a <shir95admon@gmail.com>
2021-03-29 11:25:06 +0300

    18.1

---
+++ src/Components/Text_18.1/Text.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+function Example({ string, maxLength }) {
+    const [hidden, setHidden] = useState(true);
+
+    if (string.length <= maxLength) {
+        return <p>{string}</p>
+    }
+
+    return (
+        <p>
+            {hidden ? `${string.substr(0, maxLength)} ...` : string}
+            {hidden ? ( <a style={{color:'blue'}} onClick={() => setHidden(false)}> read more</a>) : 
+            (<a style={{color:'blue'}} onClick={() => setHidden(true)}> show less</a>)}
+        </p>
+    );
+}
+
+export default Example
